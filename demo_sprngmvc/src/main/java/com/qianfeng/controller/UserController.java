0e8bf6be237fdc60fcd6811e2d2038ccb8c569c3
javen666 <1756229479@qq.com>
2019-04-09 15:27:43 +0800

    springmvc

--- demo_sprngmvc/src/main/java/com/qianfeng/controller/UserController.java
+++ demo_sprngmvc/src/main/java/com/qianfeng/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.qianfeng.controller;
 
+import com.qianfeng.bean.LoginInfoVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -19,12 +23,36 @@ public class UserController {
     /**
      * ModelAndView:给用户响应的一个对象，包括Model（数据）和要跳转的页面View
      * ModelAndView：可以完成页面跳转
+     *
+     * 默认情况：此方法既可以处理Get请求 ， 也可以处理Post请求
+     *
+     * 注解中的method属性表示请求的方式，取值有Get或者Post
      * @return
      */
-    @RequestMapping("/login")
-    public ModelAndView login(){
+//    @RequestMapping(value = "/login",method = RequestMethod.POST)
+//    @PostMapping("/login")
+    @GetMapping("/login")
+    public ModelAndView login(String username,String password){
 
         System.out.println("这是SpringMVC");
+
+        System.out.println(username + "/" + password);
+
+        ModelAndView modelAndView = new ModelAndView("hello.jsp");
+        //设置需要传递给页面的值
+        //参数1：key
+        //参数2：value
+        modelAndView.addObject("param","千锋Java");
+        return modelAndView;
+    }
+
+    @GetMapping("/login2")
+    public ModelAndView login(LoginInfoVO loginInfoVO){
+
+        System.out.println("这是SpringMVC");
+
+        System.out.println(loginInfoVO.getUsername()+"/"+loginInfoVO.getPassword());
+
 
         ModelAndView modelAndView = new ModelAndView("hello.jsp");
         //设置需要传递给页面的值
@@ -33,5 +61,18 @@ public class UserController {
         modelAndView.addObject("param","千锋Java");
 
         return modelAndView;
+    }
+
+    /**
+     *
+     * @param loginInfoVO
+     * @return 返回需要跳转的页面的地址
+     */
+    @GetMapping("/login3")
+    public String login3(LoginInfoVO loginInfoVO,Model model){
+        System.out.println("-------------->" + loginInfoVO.getUsername());
+        //值传递
+        model.addAttribute("param","千锋Java3");
+        return "hello.jsp";
     }
 }
---
+++ demo_sprngmvc/src/main/webapp/login.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yangjw
+  Date: 2019/4/9
+  Time: 14:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+<form method="get" action="login3">
+    名称：<input type="text" name="username"><br/>
+    密码：<input type="password" name="password"><br/>
+    <input type="submit">
+</form>
+</body>
+</html>
